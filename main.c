c3ba4dbae7bb7d809f6b56b014ab73fc09492ce1
mahmoud129856 <142356348+mahmoud129856@users.noreply.github.com>
2025-02-18 20:48:28 +0200

    the same

--- main.c
+++ main.c
@@ -1,55 +1,50 @@
-#include<stdio.h>
+#include <stdio.h>
 void adminSettings();
 void userSettings();
 
-typedef struct{
+typedef struct
+{
     char Id[100];
     char Name[100];
     unsigned int age;
     char address[100];
     char acountType[100];
 
-}account;
+} account;
 
-
-int main(){
-    int chouce =0;
-    while(1){
-    printf("1.Admin Moode\n");
-    printf("2.User Moode\n");
-    printf("3.Quit\n");
-    printf("Enter your choice:");
-    scanf("%d",&choice);
-    switch(choice){
-        case 1 :
-        adminSettings();
-        break;
-        case 2 :
-        userSettings();
-        break;
-        default :
-        printf("Invalid choice!\n");
-
+int main()
+{
+    int chouce = 0;
+    while (1)
+    {
+        printf("1.Admin Moode\n");
+        printf("2.User Moode\n");
+        printf("3.Quit\n");
+        printf("Enter your choice:");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            adminSettings();
+            break;
+        case 2:
+            userSettings();
+            break;
+        default:
+            printf("Invalid choice!\n");
+        }
     }
 }
 
-
-
-}
-
-void adminSettings(){
-    int chouce =0;
+void adminSettings()
+{
+    int chouce = 0;
     printf("1.create account\n");
     printf("3.update account\n");
     printf("2.delete account\n");
     printf("3.Diplay info of User\n");
-    
-    
-
-   
-
 }
 
-void userSettings(){
-
+void userSettings()
+{
 }
