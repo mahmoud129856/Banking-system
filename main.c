e699ecaa808fccf2d7331ad3a50d6225595de2e4
mahmoud129856 <142356348+mahmoud129856@users.noreply.github.com>
2025-02-18 20:47:45 +0200

    2 new files and the block of main

--- main.c
+++ main.c
@@ -1,3 +1,55 @@
-lksn;lser;oisd
-sl;dkjf;osd
-sodifjso;id
+#include<stdio.h>
+void adminSettings();
+void userSettings();
+
+typedef struct{
+    char Id[100];
+    char Name[100];
+    unsigned int age;
+    char address[100];
+    char acountType[100];
+
+}account;
+
+
+int main(){
+    int chouce =0;
+    while(1){
+    printf("1.Admin Moode\n");
+    printf("2.User Moode\n");
+    printf("3.Quit\n");
+    printf("Enter your choice:");
+    scanf("%d",&choice);
+    switch(choice){
+        case 1 :
+        adminSettings();
+        break;
+        case 2 :
+        userSettings();
+        break;
+        default :
+        printf("Invalid choice!\n");
+
+    }
+}
+
+
+
+}
+
+void adminSettings(){
+    int chouce =0;
+    printf("1.create account\n");
+    printf("3.update account\n");
+    printf("2.delete account\n");
+    printf("3.Diplay info of User\n");
+    
+    
+
+   
+
+}
+
+void userSettings(){
+
+}
