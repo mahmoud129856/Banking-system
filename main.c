f7efc55eba8c5ed79d9e5499b872906faccebf59
mahmoud129856 <142356348+mahmoud129856@users.noreply.github.com>
2025-02-18 04:57:06 +0200

    adding main file

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int a = 10;
+    int b = 20;
+    int result = a + b;
+
+    printf("Sum of %d and %d is: %d\n", a, b, result);
+
+    return 0;
+}
