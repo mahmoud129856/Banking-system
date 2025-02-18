e01298429bdc75f9cf7212c324116ef123f677c6
mahmoud129856 <142356348+mahmoud129856@users.noreply.github.com>
2025-02-18 17:58:57 +0200

    delee

--- main.c
+++ main.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
-struct
-{
-   ggggggggggggggggg
-};
-
-int main()
-{
-    int a = 10;
-    int b = 20;
-    int result = a + b;
-
-    printf("Sum of %d and %d is: %d\n", a, b, result);
-
-    return 0;
-}
+//banking project
